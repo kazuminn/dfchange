6643ae2602e480762bf68e695c915d7e196f49fc
kazuminn <warugaki.k.k@gmail.com>
2020-08-14 00:45:02 +0900

    fmt

--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
-    "github.com/kazuminn/dfchange/hash"
-    "fmt"
+	"fmt"
+	"github.com/kazuminn/dfchange/hash"
 )
 
+type config struct {
+	Path string
+	Hash []byte
+}
+
 func main() {
-    s := "sha1 this string"
+	s := "sha1 this string"
 
-    sha1 := hash.Hash_sha1{}
-    bs := sha1.GetHash(s)
+	sha1 := hash.Hash_sha1{}
+	bs := sha1.GetHash(s)
 
-
-    fmt.Printf("%x\n", bs)
+	fmt.Printf("%x\n", bs)
 }
