7c4af1f8128811776dded6e453484c90c42815cd
kazuminn <warugaki.k.k@gmail.com>
2020-08-14 01:14:02 +0900

    add code

--- main.go
+++ main.go
@@ -2,19 +2,52 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+    "path/filepath"
+    "encoding/json"
+
 	"github.com/kazuminn/dfchange/hash"
 )
 
 type config struct {
-	Path string
-	Hash []byte
+	Path string `json:"path"`
+	Hash []byte         `json:"hash"`
 }
 
 func main() {
-	s := "sha1 this string"
 
-	sha1 := hash.Hash_sha1{}
-	bs := sha1.GetHash(s)
+    root := "/home/kazumi/go/src/github.com/kazuminn/dfchange"
+    configs := []config{}
 
-	fmt.Printf("%x\n", bs)
+	err := filepath.Walk(root,
+		func(path string, info os.FileInfo, err error) error {
+			if info.IsDir() {
+				// 特定のディレクトリ以下を無視する場合は
+				// return filepath.SkipDir
+				return nil
+			}
+
+			f, err := os.Open(path)
+			if err != nil {
+				fmt.Println("can't read file")
+			}
+
+			// 一気に全部読み取り
+			b, err := ioutil.ReadAll(f)
+
+			sha1 := hash.Hash_sha1{}
+			bs := sha1.GetHash(string(b))
+			configs = append(configs, config{ path, bs, })
+			f.Close()
+
+			return nil
+		})
+
+	if err != nil {
+		fmt.Println(1, err)
+    }
+    
+    s, _ := json.Marshal(configs)
+			fmt.Println(string(s))
 }
