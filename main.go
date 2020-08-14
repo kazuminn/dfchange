8be9bfd0db5d2a0448cb378a9def9de79978aef7
kazuminn <warugaki.k.k@gmail.com>
2020-08-14 17:34:45 +0900

    refactor

--- main.go
+++ main.go
@@ -15,37 +15,39 @@ type config struct {
 	Hash []byte `json:"hash"`
 }
 
+var configs = []config{}
+
+func scan(path string, info os.FileInfo, err error) error {
+	if info.IsDir() {
+		// 特定のディレクトリ以下を無視する場合は
+		// return filepath.SkipDir
+		return nil
+	}
+
+	f, err := os.Open(path)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// 一気に全部読み取り
+	b, err := ioutil.ReadAll(f)
+
+	sha1 := hash.Hash_sha1{}
+	bs := sha1.GetHash(string(b))
+	configs = append(configs, config{path, bs})
+	f.Close()
+
+	return nil
+}
+
 func main() {
 
 	root := "/home/kazumi/go/src/github.com/kazuminn/dfchange"
-	configs := []config{}
 
-	err := filepath.Walk(root,
-		func(path string, info os.FileInfo, err error) error {
-			if info.IsDir() {
-				// 特定のディレクトリ以下を無視する場合は
-				// return filepath.SkipDir
-				return nil
-			}
-
-			f, err := os.Open(path)
-			if err != nil {
-				fmt.Println("can't read file")
-			}
-
-			// 一気に全部読み取り
-			b, err := ioutil.ReadAll(f)
-
-			sha1 := hash.Hash_sha1{}
-			bs := sha1.GetHash(string(b))
-			configs = append(configs, config{path, bs})
-			f.Close()
-
-			return nil
-		})
+	err := filepath.Walk(root, scan)
 
 	if err != nil {
-		fmt.Println(1, err)
+		fmt.Println(err)
 	}
 
 	s, _ := json.Marshal(configs)
